ac5772b77d595f7b3e6d76862ff0fe1482f981f1
kevinjclao <me@liaokev.in>
2015-11-17 09:39:05 -0500

    Page 59: Add libraries to tests.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,3 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-
-# Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+from lists.views import home_page
